f702b65c4d3d71f937797336c5348c615b3ef10a
Antonin Lefebvre <antolefe@bess-f2r1s8.clusters.42paris.fr>
2025-01-20 22:00:35 +0100

    printf tjr inexistant

--- Minitalk/minitalk/minitalk.h
+++ Minitalk/minitalk/minitalk.h
@@ -6,12 +6,13 @@
 /*   By: antolefe <antolefe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 15:06:29 by antolefe          #+#    #+#             */
-/*   Updated: 2025/01/20 18:06:18 by antolefe         ###   ########.fr       */
+/*   Updated: 2025/01/20 21:56:46 by antolefe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-# define MINITALK_H
+#define MINITALK_H
+#define _POSIX_C_SOURCE 200809L
 
 # include <stdio.h>
 # include <unistd.h>
@@ -19,6 +20,7 @@
 # include <stdlib.h>
 # include <stdbool.h>
 # include <limits.h>
+# include <sys/types.h>
 # include "../ft_printf/ft_printf.h"
 # include "../Libft/libft.h"
 
